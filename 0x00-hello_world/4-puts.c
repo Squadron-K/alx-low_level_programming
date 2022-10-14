62fd0bd227c966d69215de87d6a611bf1b092e0f
squadron <kazeemabdulkadir79@gmail.com>
2022-10-13 22:08:33 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
  /**
-  *main- Serves as the entry point for the program
+  * main- Serves as the entry point for the program
   *
-  *Return: By default returns zero if no erros otherwise returns non-zero value.
+  * Return: By default returns zero if no erros otherwise returns non-zero value.
   */
 int main(void)
 {
