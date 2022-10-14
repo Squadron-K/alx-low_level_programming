b253c1881378c1726dd254c04bf51384b56d7f33
squadron <kazeemabdulkadir79@gmail.com>
2022-10-14 10:23:17 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
 #include <unistd.h>
+
 /**
- *  *main-program entry point.
- *   *
- *    *Return: 1 on success.
- *     */
+ * main - Entry point
+ *
+ * Print long text
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
-}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}}
