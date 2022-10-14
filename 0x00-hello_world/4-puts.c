8c646ce2c91866118e9b44ffd3c8c2a6ac38d205
squadron <kazeemabdulkadir79@gmail.com>
2022-10-14 06:21:15 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
- /**
-  * main- Serves as the entry point for the program
-  *
-  * Return: By default returns zero if no erros otherwise returns non-zero value.
-  */
+
+/**
+ *  * main -Entry point
+ *   * *
+ *    * *Return: Always 0
+ *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
