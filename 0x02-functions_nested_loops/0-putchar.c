9c59522a1148f0a976429e592372888dd3a012b6
squadron <kazeemabdulkadir79@gmail.com>
2022-10-19 15:41:47 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print putchar followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	
+	{
+		printf("_putchar"\n, n);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+/**
+ * print_alphabet - Void function
+ *
+ * Return: Always (Suceess)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
