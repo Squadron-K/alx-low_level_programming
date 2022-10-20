778e10c79cc61321eaa37b1be355361163e307aa
squadron <kazeemabdulkadir79@gmail.com>
2022-10-19 22:30:14 -0700

    my commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _putchar(char c);
+int _putchar(char ch);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,3 +10,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+void putformaf(int);
