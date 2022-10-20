4ac44e377231178dea0c65adcb04700ebed6dba0
squadron <kazeemabdulkadir79@gmail.com>
2022-10-19 23:02:33 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: interger
+ * @b: integer
+ *
+ * Return: Always Result (Success)
+ */
+int add(int a, int b)
+{
+	int Result;
+
+	Result = a + b;
+	return (Result);
+}
