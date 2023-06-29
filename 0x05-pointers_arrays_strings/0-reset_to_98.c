9647f2aae89c3d8d76e7c55f6a0884fa33d6a458
Mubarak <kazeemabdulkadir79@gmail.com>
2023-06-29 10:30:30 +0000

    Solved deferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to deference parameter to 98
+ * @n: pointer to be deferenced
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
