1b8a60c2ba0dce9da2bd720f461285ed8eefaf42
Mubarak <kazeemabdulkadir79@gmail.com>
2023-06-30 08:29:40 +0000

    a function to print n number of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: Array of integers
+ * @n: number of element to be printed
+ *
+ * Return: Void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d, ", a[i]);
+	printf("%d\n", a[n - 1]);
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
-
-void puts_half(char *);
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *str;
+    int array[5];
 
-	str = "Holberton!";
-	puts_half(str);
-	return (0);
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
+    return (0);
 }
