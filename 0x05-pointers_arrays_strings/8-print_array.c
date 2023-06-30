f5cc0a467f9b14c5468c54765320c8f054cf4555
Mubarak <kazeemabdulkadir79@gmail.com>
2023-06-30 08:35:54 +0000

    a function to print n number of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,12 @@ void print_array(int *a, int n)
 {
 	int i;
 	
-	if (n <= 0)
-		return;
-
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[n - 1]);
+	if (n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", a[i]);
+		printf("%d\n", a[n - 1]);
+	}
+	else
+		printf("\n");
 }
