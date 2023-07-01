12502b1ba59747211ebae7f7073df127db61c83e
Mubarak <kazeemabdulkadir79@gmail.com>
2023-07-01 11:43:26 +0000

    strcpy implementation

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
+ * _strcpy - An implementation that copies src into dest
+ * @dest: the pointer to address to recieve values
+ * @src: An array of values
  *
+ * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
