7810da0b4d597d6876016d3279bc431033fc91b3
Mubarak <kazeemabdulkadir79@gmail.com>
2023-07-02 23:09:09 +0000

    Task 7 completed

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ */
+char *leet(char *s)
+{
+	int j, i = 0;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	while (s[i] != '\0')
+	{
+		for ( j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+		i++;
+	}
+
+	return (s);
+}
