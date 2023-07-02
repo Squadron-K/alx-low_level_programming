0d7daf6610b1cf01f77b19af1d06ba1adfb2a33e
Mubarak <kazeemabdulkadir79@gmail.com>
2023-07-02 22:34:20 +0000

    solve Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
+ * cap_string - Make first word uppercase
+ * @s: string
  *
+ * Return: value s
  */
 char *cap_string(char *s)
 {
 	int i = 0, j;
-	char op[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char op[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}'};
 
 	while (s[i] != '\0')
 	{
-		if ( i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 
 		for (j = 0; j < 13; j++)
