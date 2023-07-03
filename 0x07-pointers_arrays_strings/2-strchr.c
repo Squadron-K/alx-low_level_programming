e6e75d665ba0a121f2f99574bf09ef4731b2ec90
Mubarak <kazeemabdulkadir79@gmail.com>
2023-07-03 17:15:12 +0000

    implementation of strchr[NULL byte fix]

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
+	if (s[i] == '\0')
+		return(s);
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; s[i] <= '\0'; i++)
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-	return (" ");
+	return (NULL);
 }
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,21 +1,19 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
+char *_strchr(char *, char);
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *s = "hello";
-    char *f;
+	char *s = "First, solve the problem. Then, write the code.";
+	char *f;
 
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
+	f = _strchr(s, '\0');
+	printf("%s\n", (f == NULL ? "nil" : f));
+	return (0);
 }
