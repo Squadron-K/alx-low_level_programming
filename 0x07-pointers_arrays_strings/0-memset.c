50cbfe44847ce3b4aef16298f7bacea464950c3e
Mubarak <kazeemabdulkadir79@gmail.com>
2023-07-03 16:37:01 +0000

    Task 0 of pointer to a pointer[Betty style correction]

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,11 @@
 #include "main.h"
 /**
+ * _memset - set memory pointed to by s to b for n-bytes
+ * @s: pointer to array
+ * @b: element to add
+ * @n: number of bytes
  *
+ * Return: Pointer s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
