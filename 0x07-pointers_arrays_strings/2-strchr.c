0258fdc686a0a6bdd8403782cd223a340bc53dca
Mubarak <kazeemabdulkadir79@gmail.com>
2023-07-03 17:20:25 +0000

    implementation of strchr[NULL byte fix]

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strchr - Locates a character "c" in a string
  * @s: String
@@ -10,13 +9,12 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	if (s[i] == '\0')
-		return(s);
 
 	for (; s[i] != '\0'; i++)
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-	return (NULL);
+
+	return ("NULL");
 }
