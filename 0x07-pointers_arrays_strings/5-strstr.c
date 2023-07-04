1223545d668070742a26c3a32bc5afca7efc361f
Mubarak <kazeemabdulkadir79@gmail.com>
2023-07-04 14:51:33 +0000

    implementation of strstr[fix betty style]

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,16 +1,23 @@
 #include "main.h"
 #include <stddef.h>
 /**
+ * _strstr -Locate a substring
+ * @haystack: Main string
+ * @needle: Substring
  *
+ * Return: Located substring or NULL
  */
 char *_strstr(char *haystack, char *needle)
 {
 	int j;
 
+	if (*needle == '\0')
+		return (haystack);
+
 	for (j = 0; haystack[j] != '\0'; j++)
 	{
-		if ( needle[0] == haystack[j] && needle[1] == haystack[j + 1])
+		if (needle[0] == haystack[j] && needle[1] == haystack[j + 1])
 			return (&haystack[j]);
 	}
-	return (&haystack[0]);
+	return (NULL);
 }
